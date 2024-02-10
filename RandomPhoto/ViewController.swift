b2fb71e85917b318a2da68ea8fab8ba4b7427754
Tousif <MdTousifIslam@Tousifs-MacBook-Pro.local>
2024-02-10 17:27:56 -0600

    White box centered

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .systemPink
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
     }
 
 
